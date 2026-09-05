0676992216be1ff8d301864554d11b88504d5f69
tophi0419 <tophi0419@gmail.com>
2026-09-06 04:08:27 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Google Places API Key（請填入已限制網站來源與 Places API (New) 的 Key）
+window.GOOGLE_PLACES_API_KEY = '請填入你的Google-Places-API-Key';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='yunlin-taxi-v27';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+const C='yunlin-taxi-v30';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./config.js']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
