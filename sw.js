e2d53e1a7f13b7918191b7261269b8b0f16a8668
tophi0419 <tophi0419@gmail.com>
2026-09-05 21:42:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE='taxi-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))))
+const C='yunlin-taxi-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
